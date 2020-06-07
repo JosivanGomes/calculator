bcfe515224f45ae1641e58aeab23a00f2f881e32
JOSIVAN GOMES DA SILVA JUNIOR <josivansilvajunior@gmail.com>
2020-06-07 13:39:38 -0300

    Basic Operations and Display numbers

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -4,6 +4,8 @@ class CalcController{
 
         this._displayContentEl = document.querySelector("#content")
 
+        this._operation = []
+
         //Date and Time
         this._displayDateEl = document.querySelector("#date")
 
@@ -46,12 +48,147 @@ class CalcController{
     }
     
     //Operation's Calculator
+    setError(){
+        this.displayCalcContent = 'Error'
+    }
+    clearEntre(){
+        this._operation.pop()
+    }
+
+    clearAll(){
+        this._operation = []
+    }
+
+    deleteEntry(){
+        //APAGAR ULTIMA ENTRADA DE TEXTO
+    }
+
+    getLastEntry(){
+        return this._operation[this._operation.length - 1]
+    }
+
+    isOperator(value){
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1)
+    }
+
+    pushOperator(value){
+        if (this._operation.length == 3){
+            this._operation = [eval(this._operation.join(''))]
+            this.displayLastNumber()
+            this._operation.push(value)
+        }else{
+            this._operation.push(value)
+        }
+    }
+
+    displayLastNumber(){
+        this.displayCalcContent = this._operation[this._operation.length - 1]
+    }
+
+    addOperation(value){
+        let lastIndex = this._operation.length - 1
+
+        if (isNaN(this.getLastEntry())){
+            //OPERADOR
+            if(this.isOperator(value)){
+                if(this.isOperator(this.getLastEntry())){
+                    this._operation[lastIndex] = value
+                }else if(this._operation.length > 0){
+                    this.pushOperator(value)
+                }
+            }else if(!isNaN(value)){
+                this._operation.push(value)
+                this.displayLastNumber() 
+            }else{
+                //OUTRA COISA
+                console.log(value)
+            }
+        }else{
+            //NÚMERO
+            if(!isNaN(value)){
+                this._operation[lastIndex] += value
+                this.displayLastNumber() 
+            }else if(this._operation.length > 0){
+                this.pushOperator(value)
+            }else{
+                //OUTRA COISA
+                console.log(value)
+            }
+            
+
+        }
+        
+        console.log(this._operation)
+    }
+    
+    operationBtn(value){
+        switch (value){
+            case 'CE':
+                this.clearEntre()
+                break
+            
+            case 'C':
+                this.clearAll()
+                break
+
+            case 'Del':
+                this.deleteEntry()
+                break
+            
+            case '1/x':
+                
+                break
+
+            case 'x²':
+                
+                break
+
+            case 'sqr(x)':
+                
+                break
+
+            case '+ | -':
+                
+                break
+
+            case '=':
+                
+                break
+
+            case '.':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(value)
+                break
+
+            default:
+                this.setError()
+                break
+        }
+
+    }
+
+
     initButtons(){
         let buttons = document.querySelectorAll("#buttons > button")
 
         buttons.forEach((btn, index)=>{
             this.addEventListenerAll(btn, 'click drag', e=>{
-                console.log(btn.innerHTML)
+                let textBtn = (btn.innerHTML)
+                this.operationBtn(textBtn)
             })
 
             this.addEventListenerAll(btn, 'mouseclick mouseover mousedown', e=>{
